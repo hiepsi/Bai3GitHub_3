91a698bbf56a4e5a00b89ce16ced816559ac611e
hiepsi <hiepsi01@gmail.com>
2017-10-19 10:26:41 +0700

    Initial commit

---
+++ app/src/main/java/khuyennguyen/nguonlucxanh/bai3github_3/MainActivity.java
@@ -0,0 +1,13 @@
+package khuyennguyen.nguonlucxanh.bai3github_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
